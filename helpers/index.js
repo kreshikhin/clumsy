8a434171cf79ef4891f734ef1c21ac090fb15956
Denis Kreshikhin <denis@kreshikhin.com>
2015-10-06 17:43:46 +0300

    changed helpers

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,30 @@
 
+var path = require('path');
+
+var takeGifName = function(){
+    return path.basename(process.argv[1], '.js').replace('-build', '') + '.gif';
+}
+
+var takePngName = function(){
+    return path.basename(process.argv[1], '.js').replace('-build', '') + '.png';
+}
+
 module.exports = {
-    foo: function(){
-        console.log('bar');
+    takeGifName: takeGifName,
+    takePngName: takePngName,
+    prepareEncoder: function(ctor, canvas, name, repeat, delay, quality){
+        if(!ctor || !canvas){
+            throw('First ans second arguments are required. Pass constructor of GIFEncoder as first, and instance of Canvas as second');
+        };
+
+        var encoder = ctor(canvas.width, canvas.height);
+        encoder.createReadStream().pipe(
+            fs.createWriteStream(
+                name || takeGifName()));
+
+        encoder.setRepeat(repeat || 0);   // 0 for repeat, -1 for no-repeat
+        encoder.setDelay(delay || 50);  // frame delay in ms
+        encoder.setQuality(quality 10); // image quality. 10 is default.
+        return encoder;
     }
 }
